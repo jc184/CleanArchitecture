5374f4c22c253fc76c6a9a95d5763cd50b10bdbc
james chalmers <james.chalmers184@gmail.com>
2020-12-22 13:02:52 +0000

    Initial commit

---
+++ src/Common/Infrastructure/Files/Maps/DistrictMap.cs
@@ -0,0 +1,15 @@
+﻿using System.Globalization;
+using Application.Dto;
+using CsvHelper.Configuration;
+
+namespace Infrastructure.Files.Maps
+{
+    public class DistrictMap : ClassMap<DistrictDto>
+    {
+        public DistrictMap()
+        {
+            AutoMap(CultureInfo.InvariantCulture);
+            Map(m => m.Villages).ConvertUsing(c => "");
+        }
+    }
+}
